caf31fc2600b51c356fdcb988924b9d0c0f0ff80
MagicTang <ifredomvip@gmail.com>
2024-09-29 23:44:25 +0800

    intial

---
+++ src/api/mynote/controllers/mynote.ts
@@ -0,0 +1,7 @@
+/**
+ * mynote controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::mynote.mynote');
---
+++ src/api/mynote/services/mynote.ts
@@ -0,0 +1,7 @@
+/**
+ * mynote service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::mynote.mynote');
